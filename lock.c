c5d7415531f3eba9956e74d0ece5b2db8c3d0d43
icecold-kv <viktor3991@gmail.com>
2015-05-20 10:31:38 +0500

    corrected the code

--- lock.c
+++ lock.c
@@ -1,18 +1,34 @@
 #include <fcntl.h>
-#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[]) {
-	if (argc < 4) {
-		printf("Not enough arguments.\n");
-		return -1;
-	}
-	char path[70];
+int write_to_f (FILE *of, char *argv[]) {
 	char buf[1024];
 	strcpy(buf, "");
 	char tmp[50];
+	int strN = 1;
+	char *str = fgets(tmp, 50, of);
+	while (str != NULL) {
+		if (strN == atoi(argv[2])) {
+			strcat(buf, argv[3]);
+			strcat(buf, "\n");
+		} else
+			strcat(buf, tmp);
+		str = fgets(tmp, 50, of);
+		strN++;
+	}
+	rewind(of);
+	fwrite(buf, 1, strlen(buf), of);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 4) {
+		printf("Usage: ./lock file number newstring\n");
+		return -1;
+	}
+	char path[70];
 	struct stat st;
 	strcpy(path, argv[1]);
 	strcat(path, ".lck");
@@ -30,19 +46,7 @@ int main(int argc, char *argv[]) {
 	fclose(lck);
 	flock(of, LOCK_EX);
 	printf("Writing to file...\n");
-	int strN = 1;
-	char *str = fgets(tmp, 50, of);
-	while (str != NULL) {
-		if (strN == atoi(argv[2])) {
-			strcat(buf, argv[3]);
-			strcat(buf, "\n");
-		} else
-			strcat(buf, tmp);
-		str = fgets(tmp, 50, of);
-		strN++;
-	}
-	rewind(of);
-	fwrite(buf, 1, strlen(buf), of);
+	write_to_f(of, argv);
 	sleep(10);
 	flock(of, LOCK_UN);
 	close(of);
